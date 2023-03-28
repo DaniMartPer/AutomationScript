9334bad64667ee8459ee0bb7bf82150f8e3a3e23
Mome <Mome@DMP0205-MM>
2023-03-28 14:11:29 +0200

    Add project files.

---
+++ TestDani_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestDani_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("TestDani_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("A05D9AB2-1C38-4A2E-94C9-8D22E91979A7")]
+[assembly: AssemblyVersion("1.0.0.0")]
